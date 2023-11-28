732a5375aa09c89f1023513bf4e9ba858efec02c
Sang-Min Kang <rdrniel12345@gmail.com>
2023-11-28 20:50:12 +0900

    sangmin

--- app/src/main/java/ku/ux/scrapit/activity/MainActivity.kt
+++ app/src/main/java/ku/ux/scrapit/activity/MainActivity.kt
@@ -4,23 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.PopupWindow
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import io.realm.Realm
 import ku.ux.scrapit.R
 import ku.ux.scrapit.data.Folder
 import ku.ux.scrapit.data.IndexColor
 import ku.ux.scrapit.data.Scrap
 import ku.ux.scrapit.databinding.ActivityMainBinding
+import ku.ux.scrapit.databinding.PopupKebabMenuBinding
+import ku.ux.scrapit.etc.ScrapITApplication.Companion.currentFolderId
+import ku.ux.scrapit.etc.ScrapRVAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var currentFolder : Folder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getCurrentFolder()
+
+        binding.mainFolderTitleTv.text = currentFolder.nickname
+        binding.mainKebabBtn.setOnClickListener {
+            showKebabMenu()
+        }
         binding.mainMenuBtn.setOnClickListener {
             binding.mainDrawer.openDrawer(GravityCompat.START)
         }
@@ -29,8 +42,14 @@ class MainActivity : AppCompatActivity() {
         }
         binding.mainAddScrapBtn.setOnClickListener {
             val intent = Intent(this, AddNewItemActivity::class.java)
+            intent.putExtra("scrap", -1)
+            intent.putExtra("parentFolder", -1)
+            intent.putExtra("folder", 0)
+            intent.putExtra("url", "")
             startActivityForResult(intent, RESULT_OK)
         }
+
+        binding.mainScrapRecyclerView.adapter = ScrapRVAdapter(currentFolder.scrapList)
     }
 
     @Deprecated("Deprecated in Java")
@@ -39,6 +58,23 @@ class MainActivity : AppCompatActivity() {
         if(requestCode == RESULT_OK) {
 
         }
+    }
+
+    private fun getCurrentFolder() {
+        val realm = Realm.getDefaultInstance()
+        currentFolder = realm.where(Folder::class.java).equalTo("folderId", currentFolderId).findFirst()!!
+    }
+
+    private fun showKebabMenu() {
+        val popupBinding = PopupKebabMenuBinding.inflate(layoutInflater)
+        val popupWindow = PopupWindow(
+            popupBinding.root,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            true
+        )
+        popupWindow.elevation = 50f
+        popupWindow.showAsDropDown(binding.mainKebabBtn)
     }
 
 }
